0e90a9b5cc1558e6a974ed29355858e91626a7fc
raulDeDevWorld <proyection009@gmail.com>
2023-12-02 18:02:20 -0400

    DC

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDW2I5FIXbMDdl0lqbDBjyJtzUrbJnJKD8",
+  authDomain: "marbella-30acd.firebaseapp.com",
+  databaseURL: "https://marbella-30acd-default-rtdb.firebaseio.com",
+  projectId: "marbella-30acd",
+  storageBucket: "marbella-30acd.appspot.com",
+  messagingSenderId: "907118639667",
+  appId: "1:907118639667:web:687ace8bd9bd7ce3420146"
+};
+export const app = initializeApp(firebaseConfig);
